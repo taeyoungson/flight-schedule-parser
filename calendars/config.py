6b7398fcfe44ee976b69dd0b2b444522b5dea20a
taeyoung son <ty.son@postech.ac.kr>
2025-02-10 21:22:29 +0900

    fix: add client secret

--- calendars/config.py
+++ calendars/config.py
@@ -9,6 +9,7 @@ class GoogleSettings(pydantic_settings.BaseSettings):
     token: str = pydantic.Field(...)
     token_uri: str = pydantic.Field(...)
     client_id: str = pydantic.Field(...)
+    client_secret: str = pydantic.Field(...)
     refresh_token: str = pydantic.Field(...)
 
     model_config = pydantic_settings.SettingsConfigDict(
