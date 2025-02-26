c7f34f1e323408cc04757146e8afb9accf97c846
taeyoung son <ty.son@postech.ac.kr>
2025-02-26 22:51:01 +0900

    fix var name

--- server/dto.py
+++ server/dto.py
@@ -2,6 +2,6 @@ import pydantic
 
 
 class RegisterFlightRequest(pydantic.BaseModel):
-    result: str = pydantic.Field(..., title="ocr result over image")
+    raw_ocr_result: str = pydantic.Field(..., title="ocr result over image")
     year: int | None = pydantic.Field(None, title="year of the schedule")
     month: int | None = pydantic.Field(None, title="month of the schedule")
