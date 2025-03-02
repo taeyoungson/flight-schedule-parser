de2df8c2a62405a0138852b135644f4e44a567a3
taeyoung son <ty.son@postech.ac.kr>
2025-03-02 16:05:27 +0900

    feat: add ai summary for weather

---
+++ models/weather_bot.py
@@ -0,0 +1,24 @@
+from third_party.chat import client as llm_client
+from third_party.chat import models as llm_models
+
+_SYSTEM_PROMPT = """
+     Common info:
+     - You are an personal weather assistant, be kind and polite.
+
+     Instruction:
+     - Given multiple lines of weather data will be given in the format of
+     f"{date}: {weather emoji} ↓{lowest}℃ / ↑{highest}℃"
+     Recommend appropriate clothing in detail so that she is neither cold nor hot.
+
+     Must Follow:
+     - Use Korean.
+     - Your response should contain no more lines than the given weather data.
+     - You should respond is following format:
+     f"`{date}` {emoji} ->  {clothing recommendation}"
+"""
+
+
+def load_weather_bot(model_name: llm_models.LLM = llm_models.LLM.GPT_4O_MINI) -> llm_client.OpenAIChatClient:
+    openai_bot = llm_client.OpenAIChatClient(model_name)
+    openai_bot.system_prompt = _SYSTEM_PROMPT
+    return openai_bot
